ff42b02698958fbd361307913bdbd878acdc09bd
cjhjhj@gmail.com <cjhjhj@gmail.com>
2020-09-16 23:48:45 -0500

    fix errors

--- buildHmdbCsv.py
+++ buildHmdbCsv.py
@@ -61,8 +61,8 @@ df = df[~df["MonoisotopicMass"].isna()]
 dfDecoy = df.copy()
 proton = 1.007276466812
 for i in range(dfDecoy.shape[0]):
-    dfDecoy.loc[i, "id"] = "##Decoy_" + dfDecoy.loc[i, "id"]
-    dfDecoy.loc[i, "mass"] += 3 * proton # This way prevents 'SettingwithCopyWarning'
+    dfDecoy.loc[i, "Identifier"] = "##Decoy_" + dfDecoy.loc[i, "Identifier"]
+    dfDecoy.loc[i, "MonoisotopicMass"] += 3 * proton # This way prevents 'SettingwithCopyWarning'
 df = df.append(dfDecoy, ignore_index = True)
 csvFile = os.path.splitext(xmlFile)[0] + ".csv"
 df.to_csv(csvFile, index = False)
