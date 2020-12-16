a54af1611208f1eb69d5445eccd712ae40b6bda1
cjhjhj@gmail.com <cjhjhj@gmail.com>
2020-12-16 02:44:47 -0600

    databaseSearch is totally revised to use LSF (instead of multiprocessing in a node)

--- databaseSearch.py
+++ databaseSearch.py
@@ -1,142 +1,165 @@
-import sys, os, re, utils, pandas as pd, multiprocessing as mp, time
+import sys, os, re, math, subprocess, pickle, pandas as pd
+import utils
+from datetime import datetime
+from time import sleep
+
+# This is a wrapper for database search
+# Actual database search jobs are submitted to LSF and done using databaseSearchShell.py
 
 
-def adductDictionary(params):
-    adduct = {}
-    for key, val in params.items():
-        if key.startswith("adduct"):
-            key = re.sub(r'adduct_', '', key)
-            if key == "NH3":
-                key = "NH4" # In MetFrag, NH3 is used as NH4
-            adduct[key] = float(val)
-
-    # In MetFrag, the mass-shift is integerized
-    if params["mode"] == "1":
-        for key, val in adduct.items():
-            adduct[key] = int(val + 0.5) + 1
-        adduct["H"] = 1
-    elif params["mode"] == "-1":
-        for key, val in adduct.items():
-            adduct[key] = int(val + 0.5) - 1
-        adduct["-H"] = -1
-
-    return adduct
-
-
-def generateFiles(feature, params, precursorIonMode):
-    num = feature["feature_num"]
-    paramFile = "metfrag_params_" + str(num) + ".txt"
-    ms2File = "metfrag_data_" + str(num) + ".txt"
-    outputName = "metfrag_result_" + str(num)
-    outputFile = "metfrag_result_" + str(num) + ".csv"
-    proton = 1.007276466812
-    if params["mode"] == "1":
-        mass = feature["feature_z"] * (feature["feature_m/z"] - proton) # Neutral (monoisotopic) mass
-    elif params["mode"] == "-1":
-        mass = feature["feature_z"] * (feature["feature_m/z"] + proton) # Neutral (monoisotopic) mass
-
-    # Parameter file for MetFrag
-    f = open(paramFile, "w")
-    f.write("PeakListPath = {}\n".format(ms2File))
-
-    # Even though this code is designed to use a database including PubChem, PubChem is not available in practice due
-    # to the following reasons. 1) Many queries to PubChem through MetFrag are not stable -> problem of MetFrag (
-    # according to authors) 2) An alternative of using a local CSV file is possible, but it takes too long because of
-    # the size of file (> 30GB)
-    if params["database"].lower() == "pubchem":
-        f.write("MetFragDatabaseType = PubChem\n")
-    else:
-        if os.path.isfile(params["database"]):
-            f.write("MetFragDatabaseType = LocalCSV\n")
-            f.write("LocalDatabasePath = {}\n".format(params["database"]))
-        else:
-            sys.exit("Please check the path of a database file (.csv)")
-    f.write("DatabaseSearchRelativeMassDeviation = {}\n".format(params["mass_tolerance_formula_search"]))
-    f.write("FragmentPeakMatchRelativeMassDeviation = {}\n".format(params["mass_tolerance_ms2_peaks"]))
-    f.write("NeutralPrecursorMass = {}\n".format(mass))
-    f.write("PrecursorIonMode = {}\n".format(precursorIonMode))   # It may contain adduct information. Refer https://ipb-halle.github.io/MetFrag/projects/metfragcl/
-    if params["mode"] == "1":
-        f.write("IsPositiveIonMode = True\n")
-    elif params["mode"] == "-1":
-        f.write("IsPositiveIonMode = False\n")
-    f.write("MetFragScoreTypes = FragmenterScore\n")
-    f.write("MetFragScoreWeights = 1.0\n")
-    f.write("MetFragCandidateWriter = CSV\n")
-    f.write("SampleName = {}\n".format(outputName))
-    f.write("ResultsPath = .\n")
-    f.write("MaximumTreeDepth = 2\n")
-    f.write("MetFragPreProcessingCandidateFilter = UnconnectedCompoundFilter\n")
-    f.write("MetFragPostProcessingCandidateFilter = InChIKeyFilter\n")
+def generateShell(jobNumber, featureFile, paramFile, mem=1000, queue="gpu"):
+    binPath = r"/hpcf/authorized_apps/proteomics_apps/jumpm/python/conda/bin/python"
+    # dbSearchShell = r"/hpcf/authorized_apps/proteomics_apps/jumpm/python/current/databaseSearchShell.py"
+    dbSearchShell = r"/home/jcho/dev/JUMPm/python/current/databaseSearchShell.py"
+    filename = os.path.join(os.path.dirname(featureFile), "job_" + str(jobNumber) + ".sh")
+    f = open(filename, "w")
+    cmd = "#!/bin/bash\n"
+    cmd += "#BSUB -P prot\n"
+    cmd += "#BSUB -q {}\n".format(queue)
+    cmd += "#BSUB -R \"rusage[mem={}]\"\n".format(mem)
+    cmd += "#BSUB -e job_" + str(jobNumber) + ".e\n"
+    cmd += "#BSUB -o job_" + str(jobNumber) + ".o\n"
+    cmd += "{} {} {} {}\n".format(binPath, dbSearchShell, featureFile, paramFile)
+    f.write(cmd)
     f.close()
 
-    # MS2 data file for MetFrag
-    ms2Dict = feature["MS2"]
-    df = pd.DataFrame.from_dict(ms2Dict, orient = "columns")
-    df = df.drop([0])
-    df.to_csv(ms2File, sep = "\t", index = False, header = False)
 
-    return paramFile, ms2File, outputFile
-
-
-def runMetFrag(feature, params):
-    if feature["MS2"] is not None:
-        dfAll = pd.DataFrame()
-        adducts = adductDictionary(params)
-        for k, v in adducts.items():
-            paramFile, ms2File, outputFile = generateFiles(feature, params, v)
-
-            # MetFrag should be installed first and its path should be put to the following command
-            cmd = "java -jar " + params["metfrag"] + " " + paramFile + "> /dev/null 2>&1" # "> /dev/null 2>&1" is for linux only
-            os.system(cmd)
-            time.sleep(0.1)
-            df = pd.read_csv(outputFile)
-            if params["database"].lower() == "pubchem":
-                df = df.rename(columns = {"IUPACName": "CompoundName"})
-            df["feature_index"] = feature["feature_num"]
-            df["feature_m/z"] = feature["feature_m/z"]
-            df["feature_RT"] = feature["feature_RT"]
-            if k == "-H":
-                df["Ion"] = "[M" + str(k) + "]"
-            else:
-                df["Ion"] = "[M+" + str(k) + "]"
-            if params["mode"] == "1":
-                df["Ion"] = df["Ion"] + "+"
-            elif params["mode"] == "-1":
-                df["Ion"] = df["Ion"] + "-"
-            intensityCols = [col for col in feature.keys() if col.lower().endswith("_intensity")]
-            for c in intensityCols:
-                df[c] = feature[c]
-            columns = ["feature_index", "feature_m/z", "feature_RT"] + intensityCols + \
-                      ["Identifier", "MolecularFormula", "CompoundName", "Ion", "SMILES", "InChIKey", "FragmenterScore"]
-            df = df[columns]
-            dfAll = dfAll.append(df, ignore_index=True)
-            os.remove(paramFile)
-            os.remove(ms2File)
-            os.remove(outputFile)
-        return dfAll
-    else:
-        return None
+def checkJobStatus(jobNumbers):
+    nRemainder = len(jobNumbers)
+    while nRemainder > 0:
+        sleep(10)
+        p = subprocess.Popen("bjobs -noheader", shell=True, stdout=subprocess.PIPE)
+        (pout, _) = p.communicate()
+        pout = pout.decode().strip().split("\n")
+        jobRemainder = [p.split()[0] for p in pout]
+        nRemainder = len(set(jobNumbers).intersection(jobRemainder))
+        nFinished = len(jobNumbers) - nRemainder
+        text = "\r  {} job(s) is/are finished".format(nFinished)
+        sys.stdout.write(text)
+        sys.stdout.flush()
 
 
-def searchDatabase(features, paramFile):
-    try:
-        params = utils.getParams(paramFile)
-    except:
-        sys.exit("Parameter file cannot be found or cannot be loaded")
-    pool = mp.Pool(int(mp.cpu_count() / 2))
-    res = pool.starmap_async(runMetFrag, [(row.to_dict(), params) for idx, row in features.iterrows()])
-    nTot = res._number_left
-    progress = utils.progressBar(nTot)
-    while not res.ready():
-        nFinished = nTot - res._number_left
-        progress.increment(nFinished)
-        time.sleep(0.5)
-    res.wait()
-    progress.increment(nTot)
-    pool.close()
-    res = pd.concat(res.get(), ignore_index = True)
+def submitJobs(idx, featureFile, paramFile, memSize, queue="gpu"):
+    # Generation of .sh file (for each job)
+    generateShell(idx, featureFile, paramFile, memSize, queue)
+
+    # Submission of jobs to LSF
+    cmd = "bsub < " + "job_" + str(idx) + ".sh"
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    (pout, _) = p.communicate()
+    pout = pout.decode().strip()
+    pout = re.search("(?<=Job <)([0-9]+)", pout)
+    jobNumber = pout.group(0)
+    return jobNumber
+
+
+def searchDatabase(features, paramFile, queue="gpu"):
+    # features: fully-aligned features (pandas DataFrame)
+    # paramFile: paramter file
+
+    #################################
+    # Preparation of job submission #
+    #################################
+    m = features.shape[0]
+    n = 10  # Default number of entries in each job
+    if int(m / n) > 200:    # When there are too many features, limit the number of jobs to 200
+        n = int(m / 200) + 1
+    nJobs = math.ceil(m / n)
+
+    # Create a temporary directory for jobs (to be removed later) and change the working directory for jobs
+    cwd = os.getcwd()
+    tmpDir = os.path.join(cwd, ".tmp")
+    if os.path.exists(tmpDir):
+        os.system("rm -rf " + tmpDir)
+    os.mkdir(tmpDir)
+    os.system("cp " + paramFile + " " + tmpDir)    # Copy the parameter file to "tmpDir"
+    os.chdir(tmpDir)    # Change the working directory to a temporary one
+
+    ##################
+    # Job submission #
+    ##################
+    jobNumbers = []
+    mem = 1000  # Default memory reserved = 1000MB
+    for i in range(nJobs):
+        # Split features into "nJobs" chunks and use each chunk in each job
+        start = n * i
+        end = min(m, n * (i + 1))
+        featureFile = "features_" + str(i) + ".pickle"
+        pickle.dump(features.iloc[start:end], open(featureFile, "wb"))
+
+        # Submission of jobs to LSF
+        jobNumber = submitJobs(i, featureFile, paramFile, mem, queue)
+        jobNumbers.append(jobNumber)
+
+        text = "\r  {} job(s) is/are submitted".format(i + 1)
+        sys.stdout.write(text)
+        sys.stdout.flush()
+
+    # Check the status of submitted jobs
+    print()
+    checkJobStatus(jobNumbers)
+
+    ########################################################
+    # Check unfinished jobs (due to not enough memory) and #
+    # re-submission with the memory increase               #
+    ########################################################
+    print()
+    print("  Checking unfinished jobs")
+    isFinished = False
+    while not isFinished:
+        jobNumbers = []
+        ii = 0
+        for i in range(nJobs):
+            csvFile = "features_" + str(i) + ".csv"
+            if not os.path.exists(csvFile): # When a job is not finished properly, there's no corresponding .csv file
+                # Extracation of the required memory by parsing .o file
+                f = open("job_" + str(i) + ".o")
+                lines = f.read()
+                mem = int(re.search("(?<=Max Memory :)\s+(\d+)", lines).group(1)) * 2    # Times 2 for safety
+                f.close()
+
+                # Re-submission of jobs
+                featureFile = "features_" + str(i) + ".pickle"
+                jobNumber = submitJobs(i, featureFile, paramFile, mem, queue)
+                jobNumbers.append(jobNumber)
+
+                ii += 1
+                text = "\r  {} job(s) is/are submitted".format(ii + 1)
+                sys.stdout.write(text)
+                sys.stdout.flush()
+
+        # Check the status of submitted jobs
+        if len(jobNumbers) > 0:
+            checkJobStatus(jobNumbers)
+        else:
+            isFinished = True
+    print()
+    print("  All job(s) is/are finished")
+
+    ##########################
+    # Postprocessing of jobs #
+    ##########################
+    res = pd.DataFrame()
+    for i in range(nJobs):
+        eachOutput = "features_" + str(i) + ".csv"
+        try:
+            df = pd.read_csv(eachOutput, sep="\t")
+        except pd.errors.EmptyDataError:
+            continue
+        res = res.append(df, ignore_index=True)
+
+    ################################
+    # Generation of an output file #
+    ################################
+    os.chdir(cwd)   # Move back to the "current working directory"
+    params = utils.getParams(paramFile)
     filePath = os.path.join(os.getcwd(), "align_" + params["output_name"])
+    if not os.path.exists(filePath):
+        os.mkdir(filePath)
     outputFile = os.path.join(filePath, "align_" + params["output_name"] + ".database_matches")
-    res.to_csv(outputFile, sep = "\t", index = False)
+    res.to_csv(outputFile, sep="\t", index=False)
+
+    # os.system("rm " + os.path.join(tmpDir, "features_*"))
+    # os.system("rm " + os.path.join(tmpDir, "job_*"))
 
     return res
---
+++ databaseSearchShell.py
@@ -0,0 +1,137 @@
+import sys, os, re, math, pickle, datetime, subprocess, utils, pandas as pd
+from time import sleep
+
+
+def adductDictionary(params):
+    adduct = {}
+    for key, val in params.items():
+        if key.startswith("adduct"):
+            key = re.sub(r'adduct_', '', key)
+            if key == "NH3":
+                key = "NH4" # In MetFrag, NH3 is used as NH4
+            adduct[key] = float(val)
+
+    # In MetFrag, the mass-shift is integerized
+    if params["mode"] == "1":
+        for key, val in adduct.items():
+            adduct[key] = int(val + 0.5) + 1
+        adduct["H"] = 1
+    elif params["mode"] == "-1":
+        for key, val in adduct.items():
+            adduct[key] = int(val + 0.5) - 1
+        adduct["-H"] = -1
+
+    return adduct
+
+
+def generateFiles(feature, params, precursorIonMode):
+    num = feature["feature_num"]
+    paramFile = "metfrag_params_" + str(num) + ".txt"
+    ms2File = "metfrag_data_" + str(num) + ".txt"
+    outputName = "metfrag_result_" + str(num)
+    outputFile = "metfrag_result_" + str(num) + ".csv"
+    proton = 1.007276466812
+    if params["mode"] == "1":
+        mass = feature["feature_z"] * (feature["feature_m/z"] - proton) # Neutral (monoisotopic) mass
+    elif params["mode"] == "-1":
+        mass = feature["feature_z"] * (feature["feature_m/z"] + proton) # Neutral (monoisotopic) mass
+
+    # Parameter file for MetFrag
+    f = open(paramFile, "w")
+    f.write("PeakListPath = {}\n".format(ms2File))
+
+    # Even though this code is designed to use a database including PubChem, PubChem is not available in practice due
+    # to the following reasons. 1) Many queries to PubChem through MetFrag are not stable -> problem of MetFrag (
+    # according to authors) 2) An alternative of using a local CSV file is possible, but it takes too long because of
+    # the size of file (> 30GB)
+    if params["database"].lower() == "pubchem":
+        f.write("MetFragDatabaseType = PubChem\n")
+    else:
+        if os.path.isfile(params["database"]):
+            f.write("MetFragDatabaseType = LocalCSV\n")
+            f.write("LocalDatabasePath = {}\n".format(params["database"]))
+        else:
+            sys.exit("Please check the path of a database file (.csv)")
+    f.write("DatabaseSearchRelativeMassDeviation = {}\n".format(params["mass_tolerance_formula_search"]))
+    f.write("FragmentPeakMatchRelativeMassDeviation = {}\n".format(params["mass_tolerance_ms2_peaks"]))
+    f.write("NeutralPrecursorMass = {}\n".format(mass))
+    f.write("PrecursorIonMode = {}\n".format(precursorIonMode))   # It may contain adduct information. Refer https://ipb-halle.github.io/MetFrag/projects/metfragcl/
+    if params["mode"] == "1":
+        f.write("IsPositiveIonMode = True\n")
+    elif params["mode"] == "-1":
+        f.write("IsPositiveIonMode = False\n")
+    f.write("MetFragScoreTypes = FragmenterScore\n")
+    f.write("MetFragScoreWeights = 1.0\n")
+    f.write("MetFragCandidateWriter = CSV\n")
+    f.write("SampleName = {}\n".format(outputName))
+    f.write("ResultsPath = .\n")
+    f.write("MaximumTreeDepth = 2\n")
+    f.write("MetFragPreProcessingCandidateFilter = UnconnectedCompoundFilter\n")
+    f.write("MetFragPostProcessingCandidateFilter = InChIKeyFilter\n")
+    f.close()
+
+    # MS2 data file for MetFrag
+    ms2Dict = feature["MS2"]
+    df = pd.DataFrame.from_dict(ms2Dict, orient = "columns")
+    df = df.drop([0])
+    df.to_csv(ms2File, sep = "\t", index = False, header = False)
+
+    return paramFile, ms2File, outputFile
+
+
+def runMetFrag(feature, params):
+    if feature["MS2"] is not None:
+        dfAll = pd.DataFrame()
+        adducts = adductDictionary(params)
+        for k, v in adducts.items():
+            paramFile, ms2File, outputFile = generateFiles(feature, params, v)
+
+            # MetFrag should be installed first and its path should be put to the following command
+            cmd = "java -jar " + params["metfrag"] + " " + paramFile + "> /dev/null 2>&1" # "> /dev/null 2>&1" is for linux only
+            subprocess.call(cmd, shell=True)    # Subprocess is recommended instead of os.system
+            df = pd.read_csv(outputFile)
+            if params["database"].lower() == "pubchem":
+                df = df.rename(columns = {"IUPACName": "CompoundName"})
+            df["feature_index"] = feature["feature_num"]
+            df["feature_m/z"] = feature["feature_m/z"]
+            df["feature_RT"] = feature["feature_RT"]
+            if k == "-H":
+                df["Ion"] = "[M" + str(k) + "]"
+            else:
+                df["Ion"] = "[M+" + str(k) + "]"
+            if params["mode"] == "1":
+                df["Ion"] = df["Ion"] + "+"
+            elif params["mode"] == "-1":
+                df["Ion"] = df["Ion"] + "-"
+            intensityCols = [col for col in feature.keys() if col.lower().endswith("_intensity")]
+            for c in intensityCols:
+                df[c] = feature[c]
+            columns = ["feature_index", "feature_m/z", "feature_RT"] + intensityCols + \
+                      ["Identifier", "MolecularFormula", "CompoundName", "Ion", "SMILES", "InChIKey", "FragmenterScore"]
+            df = df[columns]
+            dfAll = dfAll.append(df, ignore_index=True)
+            os.remove(paramFile)
+            os.remove(ms2File)
+            os.remove(outputFile)
+        return dfAll
+    else:
+        return None
+
+
+########
+# Main #
+########
+featureFile = sys.argv[1]
+paramFile = sys.argv[2]
+try:
+    params = utils.getParams(paramFile)
+    features = pickle.load(open(featureFile, "rb"))
+except:
+    sys.exit("Parameter file cannot be found or cannot be loaded")
+res = pd.DataFrame()
+for idx, row in features.iterrows():
+    df = runMetFrag(row, params)
+    res = res.append(df, ignore_index=True)
+outputFile = os.path.splitext(featureFile)[0] + ".csv"
+res.to_csv(outputFile, sep="\t", index=False)
+
